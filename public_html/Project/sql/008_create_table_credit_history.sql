26ecc08c4613e804d79d7dee69cc2bdbacaf57a0
Adam Rawashdeh <ayarawashdeh@Adams-Air.home>
2022-07-25 15:49:14 -0400

    credit history

--- public_html/Project/sql/008_create_table_credit_history.sql
+++ public_html/Project/sql/008_create_table_credit_history.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `Credit_History`
     `user_id` int,
     reason varchar(15) not null COMMENT 'The type of transaction that occurred',
     details varchar(240) default null COMMENT  'Any extra details to attach to the transaction',
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
     constraint ZeroTransferNotAllowed CHECK(diff != 0),
-    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )

9e3fc8f5a84ef1d55453c6435db8d5c36f41945b
Adam Rawashdeh <ayarawashdeh@Adams-Air.home>
2022-07-27 21:00:31 -0400

    credit stuff

--- public_html/Project/sql/011_insert_credit_history.sql
+++ public_html/Project/sql/011_insert_credit_history.sql
@@ -1,1 +1,1 @@
-INSERT INTO `Credit_History`(`credit_diff`, `user_id`, reason, details) VALUES(20, 10, 'credits to user 10','giving user the ability to create a competition');
+INSERT INTO `Credit_History`(`credit_diff`, `user_id`, reason, details) VALUES(20, 10, 'credits','giving user the ability to create a competition');
